2e674e33a9d706c4acdb8679cf8b844bb626f2e8
aguadodev <aguado@edu.xunta.es>
2023-10-03 14:20:13 +0200

    Operador Ternario

---
+++ UD01/expresiones/EOE10.java
@@ -0,0 +1,23 @@
+package expresiones;
+
+import java.util.Scanner;
+
+// Programa que lea un caracter de teclado e imprima si se trata de un dígito, una mayúscula o una minúscula
+
+public class EOE10 {
+    public static void main(String[] args) {
+        char ch;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Escribe un caracter: ");
+        ch = sc.nextLine().charAt(0);
+
+        
+
+        System.out.print(ch >= '0' && ch <= '9' ? "Es un dígito" : "");
+        System.out.print(ch >= 'A' && ch <= 'Z' ? "Es una letra mayúscula" : "");
+        System.out.print(ch >= 'a' && ch <= 'z' ? "Es una letra minúscula" : "");
+
+    }
+}
