34c42cffdb566b797ed8c320b6137081e2d90f7b
aguadodev <aguado@edu.xunta.es>
2023-10-04 13:26:04 +0200

    Expresiones: 31

--- UD01/expresiones/EOE09.java
+++ UD01/expresiones/EOE09.java
@@ -24,7 +24,13 @@ public class EOE09 {
         boolean v16 = !(a >= 'A' && a <= 'Z')  &&  !(a >= 'a' && a <= 'z');
         System.out.println("16: (" + a + ") " + v16);
 
-        // Con DN, MN, AN  día, mes y año de nacimiento de una persona y DA, MA, AA día, mes y año actual, 
+
+        // 23. Una variable entera M contiene un número de mes codificado de 1 (enero) a 12 (diciembre). Comprobar si corresponde a un mes de 30 días.
+        int mes = 11;
+        boolean v23 = mes == 4 || mes == 6 || mes == 9 || mes == 11;
+        System.out.println("23: (" + mes + ") " + v23);
+
+        // 28. Con DN, MN, AN  día, mes y año de nacimiento de una persona y DA, MA, AA día, mes y año actual, 
         // escribe la expresión algorítmica que compruebe si tiene 18 años cumplidos.
         int dn = 3;
         int mn = 10;
@@ -35,7 +41,12 @@ public class EOE09 {
         boolean v28 = (aa > an + 18) || (aa - an == 18 && ma > mn) || (aa - an == 18 && ma == mn && da >= dn);
         System.out.println("28: Mayor de edad? " + v28);
 
-        // Dado un número N de dos cifras, comprobar si las dos cifras son iguales.
+        // 31. Determinar si un número entero N de 5 cifras es capicúa.
+        int n5 = 12321;
+        boolean v31 = n5 % 10 == n5 / 10000 && n5 / 10 % 10 == n5 / 1000 % 10;
+        System.out.println("31: (" + n5 + ") " + v31);
+
+        // 36. Dado un número N de dos cifras, comprobar si las dos cifras son iguales.
         boolean v36 = (n2 % 10) == (n2 / 10);
         System.out.println("36: (" + n2 + ") " + v36);
 
