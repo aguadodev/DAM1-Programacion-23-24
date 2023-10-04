cf9066629e7aa0c3508a7da1ece3912abd05a82f
aguadodev <aguado@edu.xunta.es>
2023-10-04 14:26:56 +0200

    otrosejercicios

---
+++ UD01/otrosejercicios/EP0124.java
@@ -0,0 +1,13 @@
+package otrosejercicios;
+
+public class EP0124 {
+    public static void main(String[] args) {
+        int horas = 20;
+        int minutos = 45;
+        int segundos = 34;
+
+        int totalSegundos = segundos + minutos * 60 + horas * 60 * 60;
+
+        System.out.println("Total de Segundos: " + totalSegundos);
+    }
+}
