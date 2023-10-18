c221cb13d561c273372bd3a998cfcffbc92d4591
aguadodev <aguado@edu.xunta.es>
2023-10-18 11:15:38 +0200

    E0213

---
+++ UD02/apuntes01/E0213.java
@@ -0,0 +1,38 @@
+package UD02.apuntes01;
+
+import java.util.Scanner;
+
+public class E0213 {
+    public static void main(String[] args) {
+        byte horas, minutos, segundos;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hora (0-23): ");
+        horas = sc.nextByte();
+        System.out.print("Minutos (0-59): ");
+        minutos = sc.nextByte();
+        System.out.print("Segundos (0-59): ");
+        segundos = sc.nextByte();
+
+
+        segundos++;
+        if (segundos == 60) {
+            segundos = 0;
+            minutos++;
+            if (minutos == 60) {
+                minutos = 0;
+                horas++;
+                if (horas == 24) {
+                    horas = 0;
+                }
+            }
+        }
+
+        String horaTexto = String.format("La hora incrementada en 1 segundo es %02d:%02d:%02d", horas, minutos, segundos);
+
+        System.out.println(horaTexto);
+
+
+
+    }
+}
