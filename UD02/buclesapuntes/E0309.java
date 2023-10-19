845aed43169f11565e06672cd86a5b80f09288ed
aguadodev <aguado@edu.xunta.es>
2023-10-19 13:35:43 +0200

    Bucles

--- UD02/buclesapuntes/E0309.java
+++ UD02/buclesapuntes/E0309.java
@@ -3,26 +3,27 @@ package UD02.buclesapuntes;
 import java.util.Scanner;
 
 public class E0309 {
-public static void main(String[] args) {
-    // Declaración de variables y constantes
-    int numero;
+    public static void main(String[] args) {
+        // Declaración de variables y constantes
+        int numero;
 
-    // Entrada de datos
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Escribe un número entre 1 y 10: ");
-    numero = sc.nextInt();
-    
-    // Si el número no es correcto repetimos la entrada de datos 
-    // while (numero < 1 || numero > 10) {
-    while (!(numero >= 1 && numero <= 10)) {
-        System.out.println("El número no está entre 1 y 10.");
-        System.out.println("Por favor escribe otro número: ");
+        // Entrada de datos
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un número entre 1 y 10: ");
         numero = sc.nextInt();
-    }
 
-    // Proceso & Salida
-    for (int i = 1; i <= 10; i++) {
-        System.out.println(numero + " * " + i + " = " + numero * i);
+        // Si el número no es correcto repetimos la entrada de datos
+        // while (numero < 1 || numero > 10) {
+        while (!(numero >= 1 && numero <= 10)) {
+            System.out.println("El número no está entre 1 y 10.");
+            System.out.println("Por favor escribe otro número: ");
+            numero = sc.nextInt();
+        }
+
+        // Proceso & Salida
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " * " + i + " = " + numero * i);
+        }
+        sc.close();
     }
-    
 }
