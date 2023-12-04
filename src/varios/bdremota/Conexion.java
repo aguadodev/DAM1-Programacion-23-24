2433d6cd628cb2468d943f417856b35820db5bfb
Óscar Núñez Aguado <101179757+aguadodev@users.noreply.github.com>
2023-12-04 11:15:16 +0100

    Update Conexion.java

--- src/varios/bdremota/Conexion.java
+++ src/varios/bdremota/Conexion.java
@@ -1,8 +1,8 @@
 package varios.bdremota;
 public class Conexion {
-    static final String HOST = "143.47.52.115";
-    static final String DATABASE = "damapp";
-    static final String USER = "damapp";
-    static final String PASSWORD = "DamApp-2324";
+    static final String HOST = "";
+    static final String DATABASE = "";
+    static final String USER = "";
+    static final String PASSWORD = "";
     static final String PORT = "3306";
 }
