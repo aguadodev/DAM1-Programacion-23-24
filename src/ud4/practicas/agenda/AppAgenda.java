267fa3a32d4828637c82628821572c2f0182c2b0
aguadodev <aguado@edu.xunta.es>
2024-01-23 12:37:30 +0100

    agenda con JSON

--- src/ud4/practicas/agenda/AppAgenda.java
+++ src/ud4/practicas/agenda/AppAgenda.java
@@ -1,15 +1,24 @@
 package ud4.practicas.agenda;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import com.google.gson.Gson;
+
 public class AppAgenda {
     static Persona[] agenda;
+    final static String FICHERO_AGENDA = "agenda.json";
 
     public static void main(String[] args) {
+       
         Scanner sc = new Scanner(System.in);
         // Tareas iniciales
         agenda = new Persona[0];
+        importarAgenda(FICHERO_AGENDA);
 
         // Mostrar repetidamente el menú hasta Salir
         mostrarMenu();
@@ -29,6 +38,26 @@ public class AppAgenda {
             System.out.print("Opción: ");
             opcion = sc.nextInt();
         }
+
+        exportarAgenda(FICHERO_AGENDA);
+
+    }
+
+    private static void importarAgenda(String ficheroAgenda) {
+        Gson gson = new Gson();
+        
+        String json = readFileToString(ficheroAgenda); // contenido JSON;
+        agenda = gson.fromJson(json, Persona[].class);
+
+    }
+
+    private static void exportarAgenda(String ficheroAgenda) {
+        Gson gson = new Gson();
+
+        String json = gson.toJson(agenda);      
+        
+        writeStringToFile(json, ficheroAgenda);
+        
 
     }
 
@@ -113,6 +142,62 @@ public class AppAgenda {
         System.out.println("4. Vaciar agenda");
         System.out.println("5. Buscar contacto");
         System.out.println("0. SALIR ");
-
     }
+
+    /**
+     * Crea un fichero de texto con el contenido de un String
+     * @param str
+     * @param filePath
+     */
+    public static void writeStringToFile(String str, String filePath) {
+        try {
+          // Creamos un objeto FileWriter que nos permitirá escribir en el fichero
+          FileWriter writer = new FileWriter(filePath);
+    
+          // Escribimos el String en el fichero
+          writer.write(str);
+    
+          // Cerramos el fichero
+          writer.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      
+    /**
+     * Lee y carga el contenido de un fichero de texto a un String
+     * @param filePath
+     * @return
+     */
+    public static String readFileToString(String filePath) {
+        StringBuilder fileContent = new StringBuilder();
+        try {
+            // Creamos un objeto FileReader que nos permitirá leer el fichero
+            FileReader reader = new FileReader(filePath);
+        
+            // Creamos un buffer para leer el fichero de forma más eficiente
+            BufferedReader buffer = new BufferedReader(reader);
+        
+            // Leemos el fichero línea a línea
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                // Vamos añadiendo cada línea al StringBuilder
+                fileContent.append(line);
+                // Añadimos un salto de línea al final de cada línea
+                fileContent.append("\n");
+            }
+        
+            // Cerramos el buffer y el fichero
+            buffer.close();
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("No existe el fichero.");
+            //e.printStackTrace();
+        }
+    
+        // Devolvemos el contenido del fichero como un String
+        return fileContent.toString();
+    }
+
+
 }
