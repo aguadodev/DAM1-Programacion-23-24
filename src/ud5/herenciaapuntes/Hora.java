7d267d9d237164a818b66212aef399a824cd88b3
aguadodev <aguado@edu.xunta.es>
2024-02-06 13:06:22 +0100

    Hora HoraExacta

--- src/ud5/herenciaapuntes/Hora.java
+++ src/ud5/herenciaapuntes/Hora.java
@@ -1,51 +1,104 @@
 package ud5.herenciaapuntes;
 
 public class Hora {
-    
+    byte hora;
+    byte minuto;
 
-/*
-    @Test
-    public void HoraTest() {
-        // Prueba el método inc
-        Hora hora = new Hora(23,59);
-        hora.inc();               
-        assertEquals(hora.hora, 0);
-        assertEquals(hora.minuto, 0);
-
-        Hora hora2 = new Hora(22,58);
-
-        hora2.inc();                       
-        assertEquals(hora2.hora, 22);
-        assertEquals(hora2.minuto, 59);  
-
-        hora2.inc();                       
-        assertEquals(hora2.hora, 23);
-        assertEquals(hora2.minuto, 0);  
-
-        
-        // Prueba los métodos setMinutos y setHora
-        Hora hora = new Hora(23,59);
-
-        assertFalse(hora.setMinuto(-1));
-        assertFalse(hora.setMinuto(60));
-        assertEquals(hora.minuto, 59);        
-        assertTrue(hora.setMinuto(0));
-        assertEquals(hora.minuto, 0);                
-        assertTrue(hora.setMinuto(59)); 
-        assertEquals(hora.minuto, 59);    
-               
-
-        Hora hora = new Hora(23,59);
-
-        assertFalse(hora.setHora(-1));
-        assertFalse(hora.setHora(24));
-        assertEquals(hora.hora, 23);        
-        assertTrue(hora.setHora(0));
-        assertEquals(hora.hora, 0);                
-        assertTrue(hora.setHora(23)); 
-        assertEquals(hora.hora, 23);   
-                     
+    public Hora(int hora, int minuto) {
+        this.hora = hora < 0 ? 0 : hora > 23 ? 23 : (byte) hora;
+        this.minuto = minuto < 0 ? 0 : minuto > 59 ? 59 : (byte) minuto;
     }
 
-*/    
+
+    void inc() {
+        if (minuto < 59) {
+            minuto++;
+        } else {
+            minuto = 0;
+            if (hora < 23) {
+                hora++;
+            } else {
+                hora = 0;
+            }
+        }
+    }
+
+    public boolean setHora(int hora) {
+        boolean modificado = false;
+        if (hora >= 0 && hora <= 23) {
+            this.hora = (byte) hora;
+            modificado = true;
+        }
+        return modificado;
+    }
+
+    public boolean setMinuto(int minuto) {
+        boolean modificado = false;
+        if (minuto >= 0 && minuto <= 59) {
+            this.minuto = (byte) minuto;
+            modificado = true;
+        }
+        return modificado;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d", hora, minuto);
+    }
+
+    public static void main(String[] args) {
+        Hora h = new Hora(22, 58);
+
+        for (int i = 0; i < 70; i++) {
+            h.inc(); // 0:0
+
+            System.out.println(h);
+        }
+    }
+
+    /*
+     * @Test
+     * public void HoraTest() {
+     * // Prueba el método inc
+     * Hora hora = new Hora(23,59);
+     * hora.inc();
+     * assertEquals(hora.hora, 0);
+     * assertEquals(hora.minuto, 0);
+     * 
+     * Hora hora2 = new Hora(22,58);
+     * 
+     * hora2.inc();
+     * assertEquals(hora2.hora, 22);
+     * assertEquals(hora2.minuto, 59);
+     * 
+     * hora2.inc();
+     * assertEquals(hora2.hora, 23);
+     * assertEquals(hora2.minuto, 0);
+     * 
+     * 
+     * // Prueba los métodos setMinutos y setHora
+     * Hora hora = new Hora(23,59);
+     * 
+     * assertFalse(hora.setMinuto(-1));
+     * assertFalse(hora.setMinuto(60));
+     * assertEquals(hora.minuto, 59);
+     * assertTrue(hora.setMinuto(0));
+     * assertEquals(hora.minuto, 0);
+     * assertTrue(hora.setMinuto(59));
+     * assertEquals(hora.minuto, 59);
+     * 
+     * 
+     * Hora hora = new Hora(23,59);
+     * 
+     * assertFalse(hora.setHora(-1));
+     * assertFalse(hora.setHora(24));
+     * assertEquals(hora.hora, 23);
+     * assertTrue(hora.setHora(0));
+     * assertEquals(hora.hora, 0);
+     * assertTrue(hora.setHora(23));
+     * assertEquals(hora.hora, 23);
+     * 
+     * }
+     * 
+     */
 }
--- src/ud5/herenciaapuntes/HoraExacta.java
+++ src/ud5/herenciaapuntes/HoraExacta.java
@@ -1,38 +1,83 @@
 package ud5.herenciaapuntes;
 
-public class HoraExacta {
+public class HoraExacta extends Hora {
+    byte segundo;
+
+    public HoraExacta(int hora, int minuto, int segundo) {
+        super(hora, minuto);
+        this.segundo = segundo < 0 ? 0 : segundo > 59 ? 59 : (byte) segundo;
+    }
+
+    public boolean setSegundo(int segundo) {
+        boolean modificado = false;
+        if (segundo >= 0 && segundo <= 59) {
+            this.segundo = (byte) segundo;
+            modificado = true;
+        }
+        return modificado;
+    }
+
     
+    @Override
+    void inc() {
+        if (segundo < 59) {
+            segundo++;
+        } else {
+            segundo = 0;
+            super.inc();
+        }        
+    }
 
 
 
+    @Override
+    public String toString() {
+        return String.format("%s:%02d", super.toString(), segundo);
+    }
+
+    public static void main(String[] args) {
+        HoraExacta h = new HoraExacta(23, 59, 59);
+        HoraExacta h2 = new HoraExacta(23, 59, 59);
+
+        System.out.println(h == h2);
+        System.out.println(h.equals(h2));
 
 
 
 /*
-@Test
-    public void HoraExactaTest() {
-        HoraExacta hora = new HoraExacta(23,59,59);
-        hora.inc();               
-        assertEquals(hora.hora, 0);
-        assertEquals(hora.minuto, 0);
-        assertEquals(hora.segundo, 0);
-        
-        assertFalse(hora.setMinutos(-1));
-        assertFalse(hora.setMinutos(60));
-        assertTrue(hora.setMinutos(0));        
-        assertTrue(hora.setMinutos(59));                
+        h.setSegundo(56); // 23:59:56
+        for (int i = 0; i < 10; i++){
+            h.inc();
+            System.out.println(h);
+        }*/
 
-        assertFalse(hora.setHoras(-1));
-        assertFalse(hora.setHoras(24));
-        assertTrue(hora.setHoras(0));        
-        assertTrue(hora.setHoras(23));   
-        
-        assertFalse(hora.setSegundos(-1));
-        assertFalse(hora.setSegundos(60));
-        assertTrue(hora.setSegundos(0));        
-        assertTrue(hora.setSegundos(59)); 
     }
 
-
-*/    
+    /*
+     * @Test
+     * public void HoraExactaTest() {
+     * HoraExacta hora = new HoraExacta(23,59,59);
+     * hora.inc();
+     * assertEquals(hora.hora, 0);
+     * assertEquals(hora.minuto, 0);
+     * assertEquals(hora.segundo, 0);
+     * 
+     * assertFalse(hora.setMinutos(-1));
+     * assertFalse(hora.setMinutos(60));
+     * assertTrue(hora.setMinutos(0));
+     * assertTrue(hora.setMinutos(59));
+     * 
+     * assertFalse(hora.setHoras(-1));
+     * assertFalse(hora.setHoras(24));
+     * assertTrue(hora.setHoras(0));
+     * assertTrue(hora.setHoras(23));
+     * 
+     * assertFalse(hora.setSegundos(-1));
+     * assertFalse(hora.setSegundos(60));
+     * assertTrue(hora.setSegundos(0));
+     * assertTrue(hora.setSegundos(59));
+     * }
+     * 
+     * 
+     */
 }
